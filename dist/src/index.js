0247e7c1013bb54e27571ffe636d132144646427
sonicnos <107223123+sonicnos@users.noreply.github.com>
2024-10-09 01:41:39 +0200

    make getProject and getTask

--- dist/src/index.js
+++ dist/src/index.js
@@ -11,6 +11,7 @@ const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
 /* ROUTES IMPORT */
 const projectRoutes_1 = __importDefault(require("./routes/projectRoutes"));
+const taskRoutes_1 = __importDefault(require("./routes/taskRoutes"));
 /* CONFIGURATIONS */
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
     res.send("This is home routes");
 });
 app.use("/projects", projectRoutes_1.default);
+app.use("/tasks", taskRoutes_1.default);
 /* SERVER */
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import morgan from "morgan";
 import cors from "cors";
 /* ROUTES IMPORT */
 import projectRoutes from "./routes/projectRoutes";
+import taskRoutes from "./routes/taskRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -24,6 +25,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3000;
